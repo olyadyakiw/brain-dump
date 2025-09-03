02e72dfac192fb0e419ded9df61e472df3d7714f
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-09-03 20:41:48 +0200

    add threejs-journey/12-3d-text

--- threejs_journey/12-3d-text/src/script.js
+++ threejs_journey/12-3d-text/src/script.js
@@ -1,6 +1,13 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+
+/**
+ * Font loader
+ */
+const fontLoader = new FontLoader()
 
 /**
  * Base
@@ -14,31 +21,67 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// const axisHelper = new THREE.AxesHelper()
+// scene.add(axisHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+matcapTexture.encoding = THREE.sRGBEncoding
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
 
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
+    const textGeometry = new TextGeometry('Hello Three.js', {
+        font: font,
+        size: 0.5,
+        height: 0.2,
+        curveSegments: 5,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 4,
+    })
+    // textGeometry.computeBoundingBox()
+    // textGeometry.translate(
+    //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+    // )
+    textGeometry.center()
 
-scene.add(cube)
+    const text = new THREE.Mesh(textGeometry, material)
+    scene.add(text)
+})
+
+const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material)
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+
+    scene.add(donut)
+}
 
 /**
  * Sizes
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -70,7 +113,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -80,8 +123,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
